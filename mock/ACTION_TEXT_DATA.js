e0534aea25710e1a43eb7d5c60054de46a519c4a
Sudhakar <sudhakar.jeeva7@gmail.com>
2020-06-07 02:49:13 +0530

    the rest

---
+++ mock/ACTION_TEXT_DATA.js
@@ -0,0 +1,14 @@
+const ACTION_TEXT_DATA = [
+{
+    line1:"Cost Accounting Fundas",
+},
+
+{
+    line1:"Fixed Asset Accounting",
+},
+{
+    line1:"Non-Profit Accounting",
+},
+];
+
+export default ACTION_TEXT_DATA;
